878140a0fa675a528f58c7d986a1c5365974f129
Patrício Rios <patricio.osterno1@gmail.com>
2023-02-09 14:18:24 -0300

    Exercício básico de switch-case

---
+++ src/aulasdescomplica/Switch.java
@@ -0,0 +1,20 @@
+
+package aulasdescomplica;
+
+public class Switch {
+    public static void main(String[] args) {
+        int numero = 3;
+        
+        switch(numero) {
+            case 1:
+                System.out.println("caso 1");
+                break;
+            case 2:
+                System.out.println("caso 2");
+                break;
+            case 3:   
+                System.out.println("caso 3");
+                break;
+        }
+    }
+}
